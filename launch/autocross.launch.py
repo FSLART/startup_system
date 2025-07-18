c3c2f090cc802f21897fb94e24583c22756ef699
João Roxo <2221411@my.ipleiria.pt>
2025-07-18 14:26:41 +0100

    Launch files for each mission

---
+++ launch/autocross.launch.py
@@ -0,0 +1,38 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config_file = os.path.join(
+        get_package_share_directory('safety_monitor'),
+        'config',
+        'default_params.yaml'
+    )
+    return LaunchDescription([
+        Node(
+            package='safety_monitor',
+            executable='monitor_node',
+            name='SafetyMonitorNode',
+            parameters=[config_file],
+            output='screen'
+        ),
+        #Node(
+         #   package='zed_bridge',
+         #   executable='zed_bridge',
+         #   name='zed_bridge',
+         #   output='screen',
+        #),
+        Node(
+            package='ekf_node',
+            executable='ekf_node',
+            name='ekf_node',
+            output='screen',
+        ),
+        Node(
+            package='spac2_0',
+            executable='spac_node',
+            name='DriveModelNode',
+            output='screen'
+        ),
+    ])
